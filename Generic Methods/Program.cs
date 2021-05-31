5de4b2d2e36e390e44da50d3663aacb246a25679
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-31 21:36:27 +0530

    Add files via upload

---
+++ Generic Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generic_Method
+{
+  
+        class Program
+        {
+            //Generic method  
+            static void Swap<T>(ref T a, ref T b)
+            {
+                T temp;
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            static void Main(string[] args)
+            {
+                // Swap of two integers.  
+                int a = 40, b = 60;
+                Console.WriteLine("Before swap: {0}, {1}", a, b);
+
+                Swap<int>(ref a, ref b);
+
+                Console.WriteLine("After swap: {0}, {1}", a, b);
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+
